3102af6a7a8a48a43f9b4e68170e72d4de613649
Ang Ming Liang <angmingliang4017ic@gmail.com>
2017-04-06 00:27:43 +0800

    Updated

--- featuriser.py
+++ featuriser.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python
+
+#In_Built Libaries
 import os
+import glob
+from multiprocessing import Pool
+
+#Data Analysis
 import numpy as np
+import pandas as pd
+
+#Image Processing
 import dicom
 import cv2
+
+#Machine Learning
 import mxnet as mx
-import pandas as pd
-from multiprocessing import Pool
 
 img_rows = 512
 img_cols = 512
 smooth = 1.
-
-files = [i + '.npy' for i in pd.read_csv('./missing.csv')['id']]
-
 
 def get_extractor():
     model = mx.model.FeedForward.load('./resnet-50', 0, ctx=mx.cpu(), numpy_batch_size=1)
@@ -31,9 +38,12 @@ def calc_features(image):
 
     feats = net.predict(batch)
     print(feats.shape)
-    np.save('./out/preprocessed/{}'.format(files[id_]), feats)
+    np.save('./train_features/{}'.format(files[id_]), feats)
     return feats
 
 if __name__ == '__main__':
+    with open("./settings.json") as data_file:
+      data = json.load(data_file)
+    INPUT_FOLDER = data["data"]["raw"]["DSB"]
     p = Pool(processes=15)
-    p.map(calc_features, range(2, len(files)))
+    p.map(calc_features, glob.glob(INPUT_FOLDER))
