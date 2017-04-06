8c32530f16921aedf8a5f97ab68e03ec1078ed20
Ang Ming Liang <angmingliang4017ic@gmail.com>
2017-04-06 15:16:35 +0800

    Updated

--- xgboost_model.py
+++ xgboost_model.py
@@ -18,6 +18,8 @@ import cv2
 #Machine Learning Models
 import mxnet as mx
 from sklearn.model_selection import StratifiedKFold
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 from scipy.stats import gmean
 import xgboost as xgb
 
@@ -26,14 +28,19 @@ img_cols = 512
 
 smooth = 1.
 
-def train_xgboost(labels,features):
-    df = pd.read_csv('./stage1_labels.csv')
+def train_xgboost(labels,features,model):
+    df = pd.read_csv(labels)
     print(df.head())
     
-    x = np.array([np.mean(np.load('./stage1.1_features/%s.npy' % str(id)), axis=0) for id in df['id'].tolist()])
-    print(x.shape)
+    x = np.array([np.mean(np.load(features + '/%s.npy' % str(id)), axis=0) for id in df['id'].tolist()])
+    
+    #PCA and feature selection / We have not tried the TSNE one yet
+    X_std = StandardScaler().fit_transform(x)
+    x_pca = PCA(n_components = 13).fit(X_std)
+    
     y = df['cancer'].as_matrix()
     
+    #Stratified Cross-Validation
     skf = StratifiedKFold(n_splits=5, random_state=2048, shuffle=True)
     
     result =[]
@@ -41,27 +48,31 @@ def train_xgboost(labels,features):
     for train_index, test_index in skf.split(x, y):
         trn_x, val_x = x[train_index,:], x[test_index,:]
         trn_y, val_y = y[train_index], y[test_index]
-
+        
+        #XGBoost Model
         #clf = xgb.XGBRegressor(max_depth=5, n_estimators=2500, min_child_weight=96, learning_rate=0.03756, nthread=8, subsample=0.85, colsample_bytree=0.9, seed=96)
         clf = xgb.XGBRegressor(max_depth=5, n_estimators=2500, min_child_weight=96, learning_rate=0.03757, nthread=8, subsample=0.85, colsample_bytree=0.9, seed=96)
         # max_depth=5 Public score = ?
         # max_depth=4 Public score = 0.54721
         # max_depth=3 Public score = 0.55193
-
+        
+        #Training Model
         clf.fit(trn_x, trn_y, eval_set=[(val_x, val_y)], verbose=True, eval_metric='logloss', early_stopping_rounds=100)
         result.append(clf.best_score)
         clfs.append(clf)
 
-    pickle.dump(clf, open("xgboostmodel.pkl", "wb"))
+    pickle.dump(clf, open(model, "wb"))
     return result
 
 def xgboostModel(sample,model):
+    #Opening up model
     try:
         clf = pickle.load(open(model, "rb"))
         print("Opening Model")
     except:
         clf = train_xgboost()
-
+    
+    #Make predicitions
     df = pd.read_csv(sample)
     pred = []
     for id_ in df['id'].tolist():
@@ -69,6 +80,7 @@ def xgboostModel(sample,model):
                 x = np.mean(np.load(sample+'%s.npy' % str(id_)), axis=0)
                 pred.append(clf.predict(np.array([x]))[0])
             except FileNotFoundError:
+                #0.259126 is the mean in that population of test data that a person has cancer
                 pred.append(0.25912670007158195)
     
     df['cancer'] = pred
