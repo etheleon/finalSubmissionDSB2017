1bd996d74408778d1eaa08bd2196c4cf85e4c692
Ang Ming Liang <angmingliang4017ic@gmail.com>
2017-04-06 14:30:41 +0800

    Update

--- predict.py
+++ predict.py
@@ -1,33 +1,15 @@
 #In_Built Library
 import os
-import pickle
 import glob
-
 import json
 
 #Data Analysis 
 import numpy as np
-from matplotlib import pyplot as plt
 import pandas as pd
-
-#Image Analysis
-import dicom
-import cv2
-
-#Machine Learning
-import mxnet as mx
-from sklearn import cross_validation
-import xgboost as xgb
 
 #Custom Modules
 import BloodModel
 import xgboost_model
-
-img_rows = 512
-img_cols = 512
-
-smooth = 1.
-
 
 def predict(sample,model,predictions):
     xgboost(sample,model)
