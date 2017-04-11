231d10c6c01d538f651d76a3d22f98a22af0ed43
etheleon <picy2k@gmail.com>
2017-04-11 23:31:11 +0800

    Featuriser for masked

--- featuriser.py
+++ featuriser.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 
 #In_Built Libaries
-import os
 import glob
-import time
-from multiprocessing import Pool
+import multiprocessing as mp
 
 #Data Analysis
 import numpy as np
 import pandas as pd
-
-#Image Processing
-import dicom
-import cv2
 
 #Machine Learning
 import mxnet as mx
@@ -22,31 +16,30 @@ img_cols = 512
 smooth = 1.
 
 def get_extractor():
-    model = mx.model.FeedForward.load('./resnet-50', 0, ctx=mx.cpu(), numpy_batch_size=1)
+    model = mx.model.FeedForward.load('/w/data/one/stage1/resnet-50', 0, ctx=mx.cpu(), numpy_batch_size=1)
     fea_symbol = model.symbol.get_internals()["flatten0_output"]
-    feature_extractor = mx.model.FeedForward(ctx=mx.cpu(), symbol=fea_symbol, numpy_batch_size=64,
-                                             arg_params=model.arg_params, aux_params=model.aux_params,
-                                             allow_extra_params=True)
-
+    feature_extractor = mx.model.FeedForward(ctx=mx.cpu(), symbol=fea_symbol,
+    numpy_batch_size=64, arg_params=model.arg_params, aux_params=model.aux_params,
+    allow_extra_params=True)
     return feature_extractor
 
-net = get_extractor()
-
-def calc_features(image):
-    start_time = time.time()
-    batch = np.mean(image, axis=0)
-    batch = np.array([batch])
-    print("{} is Found ".format(str(id_)))
-
-    feats = net.predict(batch)
-    # print(feats.shape)
-    np.save('./train_features/{}'.format(files[id_]), feats)
-    print("Generating is finished in --- %s seconds ---" % (time.time() - start_time))
-    return feats
+def readFiles(file):
+    try:
+        print("Reading {}".format(file))
+        image = np.load(file)
+        summed = np.mean(image, axis=0)
+        return (file, summed)
+    except:
+        print("This file is problematic: {}".format(file))
 
 if __name__ == '__main__':
-    with open("./settings.json") as data_file:
-      data = json.load(data_file)
-    INPUT_FOLDER = data["data"]["raw"]["DSB"]
-    p = Pool(processes=15)
-    p.map(calc_features, glob.glob(INPUT_FOLDER))
+    net = get_extractor()
+    files = glob.glob("/w/data/two/stage2/data/*.npy")
+    results= mp.Pool(20).map(readFiles, files)
+    names = [i[0] for i in results]
+    batchArrays = [i[1] for i in results]
+    fullstack = np.stack(batchArrays, axis=0)
+    feats = net.predict(fullstack)
+    df = pd.DataFrame(np.stack(feats, axis=0))
+    df["filenames"] = names
+    df.to_csv("/w/data/two/stage2/reprocessed_features.csv")
